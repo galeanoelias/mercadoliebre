a37a776eae7ccec42c2adc8886d673f20691f34c
Galeano Elias <galeanoeliasdavid@gmail.com>
2021-11-15 15:58:54 -0300

    commit m. liebre

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.use(express.static("public"));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
 
 app.listen(PORT, () => console.log(`
 Servidor levantado en el puerto ${PORT}
